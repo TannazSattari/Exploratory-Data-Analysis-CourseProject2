51d75cf882f8ed9e40fd4a40421c5528661c87c4
Tannaz Sattari Tabrizi <t.sattari@gmail.com>
2016-05-04 21:59:10 -0500

    Question 4

---
+++ Plot4.R
@@ -0,0 +1,24 @@
+#Q4-Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+
+#Make sure that summarySCC_PM25.rds and Source_Classification_Code.rds is in your working directory
+#Reading the data
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#merging two data
+data1<-merge(NEI, SCC, by="SCC")
+
+#subsetting the coal combustion-related sources data
+hasCoal<-grepl("coal",data1$Short.Name,ignore.case = TRUE)
+coalSubData<-data1[hasCoal,]
+
+# calculating the total emissions from PM2.5 for each year
+data <- tapply(coalSubData$Emissions,coalSubData$year,FUN=sum)
+
+as.data.frame(data)
+
+png("plot4.png")
+barplot(data,xlab = "year",ylab="Total emissions from PM2.5",col=3,main = "Total emissions from coal combustion-related sources \n in the Baltimore City, Maryland from 1999 to 2008" )
+dev.off()
+
